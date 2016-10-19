a332e36ce6cf0ed0bf4c1c7d3528e6b2967dace7
Simon Bennett <simon@simnom.co.uk>
2016-10-19 11:34:10 +0100

    Hey Ryan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hey Ryan!
                 </div>
 
                 <div class="links">
