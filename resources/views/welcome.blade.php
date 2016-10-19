1d27b9f94206a2fdf180350a3a487ed6fbe478a9
Simon Bennett <simon@simnom.co.uk>
2016-10-19 22:09:10 +0100

    Just for fun

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hey Ryan again!
+                    Playing at home
                 </div>
 
                 <div class="links">
